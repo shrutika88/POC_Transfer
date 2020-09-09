38cd232ab9bbfebf7f4a74986043326e36a5c635
Yadav <Shrutika_Yadav@spe.sony.com>
2020-09-09 14:39:09 +0530

    test case added

--- src/test/java/com/example/calculator/addition/AdditionApplicationTests.java
+++ src/test/java/com/example/calculator/addition/AdditionApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.calculator.addition;
 
+import junit.framework.Assert;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,11 @@ class AdditionApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	Addition ad = new Addition();
+	@Test
+	public void testAdd(){
+		Assert.assertEquals(9, ad.add(4,5));
 	}
 
 }
--- src/test/java/com/example/calculator/addition/AdditionTest.java
+++ src/test/java/com/example/calculator/addition/AdditionTest.java
@@ -3,13 +3,15 @@ package com.example.calculator.addition;
 import junit.framework.Assert;
 
 import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
 public class AdditionTest {
 	
 	Addition ad = new Addition();
 	
 	@Test
 	public void testAdd(){
-		Assert.assertEquals(9, ad.add(4,5));
+		Assert.assertEquals(9, ad.add(4,4));
 	}
 	
 	
